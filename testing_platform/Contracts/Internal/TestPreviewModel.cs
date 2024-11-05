bb97f126632aa386ea2b49945710a97cc7de1f18
sviatoslavchaikovskyi <swiatik12@gmail.com>
2024-11-05 02:06:56 +0200

    test preview fe, update Radzen to 5.5.3

---
+++ testing_platform/Contracts/Internal/TestPreviewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Contracts.Internal;
+
+public class TestPreviewModel
+{
+    public bool IsPremium { get; set; }
+    public string Caption { get; set; } = null!;
+    public DateOnly UploadDate { get; set; }
+    public byte DurationInMinutes { get; set; }
+    public string Category { get; set; } = null!;
+    public string Complexity { get; set; } = null!;
+    public List<string> Tags { get; set; } = null!;
+    public bool IsCompleted { get; set; }
+    public bool IsFavourite { get; set; }
+}
